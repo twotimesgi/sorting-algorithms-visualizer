4216127f043a863032ad28d96e7d216362208382
twotimesgi <luigibardellagerbi@mail.com>
2022-03-12 17:33:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        array: [],
+        max: 0,
+        timer: null,
+        n: 100,
+        i: -2,
+        j: -2,
+        delay: 10,
+        isRunning: false,
+        isSorted: false
+    },
+    methods: {
+        getArray() {
+            this.isRunning = false;
+            this.i = -2;
+            this.j = -2;
+            this.max = 0;
+            this.array = [];
+            for (this.i = 0; this.i < this.n; this.i++) {
+                let rand = Math.floor(Math.random() * this.n + 1);
+                if (rand > this.max) this.max = rand;
+                this.array.push(rand);
+            }
+        },
+        async bubbleSort() {
+            this.isRunning = true;
+            // BubbleSort Algorithm
+            for (this.i = 0; this.i < this.array.length && this.isRunning; this.i += 1) {
+                for (this.j = 0; this.j < this.array.length - this.i - 1 && this.isRunning; this.j += 1) {
+        
+                    // To wait for .1 sec
+                    await new Promise((resolve) =>
+                        setTimeout(() => {
+                            resolve();
+                        }, 20)
+                    );
+                    
+                    let value1 = this.array[this.j];
+                    let value2 = this.array[this.j+1];
+        
+                    // To compare value of two blocks
+                    if (value1 > value2) {
+                        await new Promise((resolve) => {
+                                // For waiting for .25 sec
+                                setTimeout(() => {
+                                    let temp = this.array[this.j];
+                                    this.array[this.j]= this.array[this.j+1];
+                                    this.array[this.j+1] = temp;
+                                    this.$forceUpdate();
+                                    resolve();
+                                }, 150);
+                        });
+                    }
+                }
+            }
+        },
+        startSort(){
+            if(!this.isRunning){
+                this.bubbleSort();
+            }
+        },
+        stopSort(){
+            this.isRunning = false;
+        }
+    },
+    created() {
+        this.getArray();
+        }
+    });
